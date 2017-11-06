a003db8441ba47f225bfb3d8345aed6bc029e817
heybluguy <heybluguy@yahoo.com>
2017-11-06 13:16:18 -0700

    pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,13 @@
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = {}
+
+  end
+
+  def stock_check(item)
+    
+  end
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,21 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  attr_reader :pantry
 
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_a_stock
+    assert_instance_of Hash, pantry.stock
+  end
+
+  def test_can_stock_items
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
 end
